46405ff60fe10caec4cf66a04ea416dd4862ecf6
NakagawaRen <ren.nakagawa.r@gmail.com>
2023-07-16 17:21:42 +0900

    fix run.py date

--- run.py
+++ run.py
@@ -22,11 +22,12 @@ def parse_args():
 
 def main():
     args = parse_args()
-    os.makedirs(f"result/{datetime.datetime.now().strftime('%m%d')}_{args.path}")
-    os.makedirs(f"result/{datetime.datetime.now().strftime('%m%d')}_{args.path}/raw", exist_ok=True)
-    os.makedirs(f"result/{datetime.datetime.now().strftime('%m%d')}_{args.path}/processed", exist_ok=True)
+    date = datetime.datetime.now().strftime('%m%d')
+    os.makedirs(f"result/{date}_{args.path}")
+    os.makedirs(f"result/{date}_{args.path}/raw", exist_ok=True)
+    os.makedirs(f"result/{date}_{args.path}/processed", exist_ok=True)
     main = run_command(f"python3 {args.path}.py > result/{datetime.datetime.now().strftime('%m%d')}_{args.path}/raw/experiment.log")
-    rp(f"{datetime.datetime.now().strftime('%m%d')}_{args.path}")
+    rp(f"{date}_{args.path}")
 
 
 if __name__ == "__main__":
