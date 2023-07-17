45b7150e14d7f154649a6edbb2a82338d438b4d5
NakagawaRen <ren.nakagawa.r@gmail.com>
2023-07-17 15:44:45 +0900

    es imp

--- run.py
+++ run.py
@@ -31,15 +31,20 @@ def main():
     while os.path.exists(f"result/{date}_{args.path}_{idx}"):
         idx += 1
     dirname = f"result/{date}_{args.path}_{idx}"
+    print(f"{date}_{args.path}_{idx} running...")
     os.makedirs(dirname)
     os.makedirs(f"{dirname}/raw", exist_ok=True)
     os.makedirs(f"{dirname}/processed", exist_ok=True)
     cp_lib = run_command(f"cp -r lib/ {dirname}/raw/")
     cp_main = run_command(f"cp {args.path}.py {dirname}/raw/")
+    print("setup done")
     main = run_command(
         f"python3 {args.path}.py > {dirname}/raw/experiment.log"
     )
+    print("main done")
     rp(dirname)
+    print("result done")
+    print(f"{date}_{args.path}_{idx} done")
 
 
 if __name__ == "__main__":
