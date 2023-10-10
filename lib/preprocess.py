9fb1184dd8dd5a187c15c616f60617708b812966
NakagawaRen <ren.nakagawa.r@gmail.com>
2023-10-10 10:38:48 +0900

    set 2 hour in optuna search time

--- lib/preprocess.py
+++ lib/preprocess.py
@@ -71,7 +71,7 @@ def load_preprocessed_data(LABELS, TIME_PERIODS, STEP_DISTANCE, LABEL, N_FEATURE
         def _gaussian(x, sigma):
             return np.exp(-np.power(x, 2.) / (2 * np.power(sigma, 2.)))
         def _pad(x, k):
-            return np.pad(x, (k, k), 'constant', constant_values=(0, 0))
+            return np.pad(x, (k), 'constant', constant_values=(0, 0))
         def _convolve(x, w):
             return np.array([np.convolve(x[0], w, mode='valid'), np.convolve(x[1], w, mode='valid'), np.convolve(x[2], w, mode='valid')])
         inputs = copy.deepcopy(data)
@@ -83,7 +83,7 @@ def load_preprocessed_data(LABELS, TIME_PERIODS, STEP_DISTANCE, LABEL, N_FEATURE
 
     def _median_filter(data, k=5):
         def _pad(x, k):
-            return np.pad(x, (k, k), 'constant', constant_values=(0, 0))
+            return np.pad(x, (k), 'constant', constant_values=(0, 0))
         def _convolve(x, w):
             return np.array([np.convolve(x[0], w, mode='valid'), np.convolve(x[1], w, mode='valid'), np.convolve(x[2], w, mode='valid')])
         inputs = copy.deepcopy(data)
