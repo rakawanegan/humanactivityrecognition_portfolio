2d58c3db2c0f38466ff6d53b8486545953eb1957
NakagawaRen <ren.nakagawa.r@gmail.com>
2023-10-15 14:26:18 +0900

    add all input diff

--- lib/model.py
+++ lib/model.py
@@ -10,7 +10,7 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(p=dropout)
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, dim, 2) * (-torch.log(torch.tensor(10000.0)) / dim))
-        pe = torch.zeros(max_len, 1, dim)
+        pe = torch.zeros(max_len, dim)
         pe[:, 0, 0::2] = torch.sin(position * div_term)
         pe[:, 0, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
