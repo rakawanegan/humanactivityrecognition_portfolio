054330833a95b18a7548d5b7fdc7d02d519c70d9
NakagawaRen <ren.nakagawa.r@gmail.com>
2023-10-15 14:27:09 +0900

    add all input diff

--- lib/model.py
+++ lib/model.py
@@ -11,8 +11,8 @@ class PositionalEncoding(nn.Module):
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, dim, 2) * (-torch.log(torch.tensor(10000.0)) / dim))
         pe = torch.zeros(max_len, dim)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
