42bd69998d8e1f0ad125c5bdd209f8cadb1f2af1
NakagawaRen <ren.nakagawa.r@gmail.com>
2023-10-18 15:08:42 +0900

    add cnn1d

--- error_bar.py
+++ error_bar.py
@@ -2,17 +2,42 @@ import copy
 import datetime
 import os
 
-import joblib
 import numpy as np
 import pandas as pd
 import torch
-from matplotlib import pyplot as plt
 from torch import nn, optim
 from torch.utils.data import DataLoader
 
-from lib.model import PreConvTransformer
+from keras.layers import Conv1D, Dense, Dropout, GlobalMaxPooling1D
 from lib.preprocess import load_data
-from lib.local_utils import send_email, is_worse, SeqDataset
+from lib.local_utils import is_worse, SeqDataset
+
+import datetime
+import os
+
+import joblib
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+
+from lib.preprocess import load_data
+import datetime
+import os
+
+import joblib
+import keras
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+from keras.layers import Conv1D, Dense, Dropout, GlobalMaxPooling1D
+from keras.models import Model, Sequential
+from keras.utils.np_utils import to_categorical
+from matplotlib import pyplot as plt
+from scipy import stats
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+from lib.preprocess import load_data
 
 
 MODEL_NAME = "error_bar"
@@ -33,7 +58,7 @@ LABEL = "ActivityEncoded"
 # set random seed
 SEED = 314
 
-dirname = f"result/{MODEL_NAME}"
+dirname = f"result/{MODEL_NAME}/cnn1d"
 
 x_train, x_test, y_train, y_test = load_data(
     LABELS, TIME_PERIODS, STEP_DISTANCE, LABEL, N_FEATURES, SEED
@@ -50,97 +75,36 @@ print("Device: ", device)
 print("Max Epochs: ", MAX_EPOCH)
 print("Early Stopping Reference Size: ", REF_SIZE)
 
-adm_params = {
-    "lr": 0.0001,
-    "betas": (0.9, 0.999),
-    "eps": 1e-08,
-    "weight_decay": 0,
-    "amsgrad": False,
-}
-
-calr_params = {
-    "T_max": 150,
-    "eta_min": 1e-05,
-    "last_epoch": -1,
-    "verbose": False,
-}
-
-pct_params = {
-    "num_classes": len(LABELS),
-    "input_dim": TIME_PERIODS,
-    "channels": N_FEATURES,
-    "hidden_ch": 25,
-    "hidden_dim": 1024,
-    "depth": 5,
-    "heads": 8,
-    "mlp_dim": 1024,
-    "dropout": 0.01,
-    "emb_dropout": 0.01,
-}
-model = PreConvTransformer(**pct_params).to(device)
-
-loss_function = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), **adm_params)
-scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, **calr_params)
-
-
-train = SeqDataset(torch.from_numpy(x_train).float(), torch.from_numpy(y_train).float())
-test = SeqDataset(torch.from_numpy(x_test).float(), torch.from_numpy(y_test).float())
-train_loader = DataLoader(
-    train, batch_size=BATCH_SIZE, shuffle=True, num_workers=os.cpu_count()
+model = Sequential()
+model.add(
+    Conv1D(160, 12, input_shape=(x_train.shape[1], x_train.shape[2]), activation="relu")
 )
-test_loader = DataLoader(
-    test, batch_size=BATCH_SIZE, shuffle=False, num_workers=os.cpu_count()
+model.add(Conv1D(128, 10, activation="relu"))
+model.add(Conv1D(96, 8, activation="relu"))
+model.add(Conv1D(64, 6, activation="relu"))
+model.add(GlobalMaxPooling1D())
+model.add(Dropout(0.5))
+model.add(Dense(6, activation="softmax"))
+
+print(model.summary())
+
+model.compile(
+    loss="categorical_crossentropy", optimizer="rmsprop", metrics=["accuracy"]
 )
 
-losslist = list()
+epochs = 150
+batch_size = 1024
+history = model.fit(
+    x_train,
+    y_train,
+    validation_data=(x_test, y_test),
+    epochs=epochs,
+    batch_size=batch_size,
+)
 
-ep_start = datetime.datetime.now()
-best_model = copy.deepcopy(model)
-for ep in range(1, MAX_EPOCH + 1):
-    losses = list()
-    for batch in train_loader:
-        x, t = batch
-        x = x.to(device)
-        t = t.to(device)
-        model.train()
-        optimizer.zero_grad()
-        output = model(x)
-        loss = loss_function(output, t)
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
-    # scheduler.step()
-    ls = np.mean(losses)
-    if ep > calr_params["T_max"]:
-        if min(losslist) > ls:
-            best_model = copy.deepcopy(model)
-        if is_worse(losslist, REF_SIZE, "minimize"):
-            print(f"early stopping at epoch {ep} with loss {ls:.5f}")
-            break
-    print(f"Epoch {ep + 0:03}: | Loss: {ls:.5f}")
-    losslist.append(ls)
-    if ep == 1:
-        ep_delta = datetime.datetime.now() - ep_start
-        print(f"Estimated time: {ep_delta * MAX_EPOCH}")
-        print(f"Estimated finish: {datetime.datetime.now() + ep_delta * MAX_EPOCH}")
-        # send_email(
-        #     "Training started",
-        #     f"Training started at {ep_start.strftime('%Y-%m-%d %H:%M:%S')}\nEstimated time: {ep_delta * MAX_EPOCH}\nEstimated finish: {datetime.datetime.now() + ep_delta * MAX_EPOCH}"
-        # )
-model = best_model
+y_pred = model.predict(x_test)
+y_test = y_test.argmax(axis=-1)
 
-
-model.eval()
-y_pred = list()
-for batch in test_loader:
-    x, _ = batch
-    x = x.to(device)
-    optimizer.zero_grad()
-    output = model(x)
-    y_pred.append(output.detach().cpu().numpy())
-
-y_pred = np.concatenate(y_pred, axis=0)
 y_pred = pd.DataFrame(y_pred)
 y_test = y_test.argmax(axis=-1)
 y_pred["true"] = y_test
