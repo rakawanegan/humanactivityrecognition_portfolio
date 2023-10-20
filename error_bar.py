695722a8ef207fdce0cc12d49d953a5e44625e28
NakagawaRen <ren.nakagawa.r@gmail.com>
2023-10-20 12:01:24 +0900

    add cnn1d(softmax->liner)

--- error_bar.py
+++ error_bar.py
@@ -1,32 +1,8 @@
-import copy
-import datetime
-import os
-
-import numpy as np
-import pandas as pd
-import torch
-from torch import nn, optim
-from torch.utils.data import DataLoader
-
-from lib.model import ConvolutionalNetwork as CNN1D
 from lib.preprocess import load_data
-from lib.local_utils import SeqDataset
-
 import datetime
-import os
-
-import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-
-from lib.preprocess import load_data
-import datetime
-import os
-
-import joblib
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-
-from lib.preprocess import load_data
+from keras.layers import Conv1D, Dense, Dropout, GlobalMaxPooling1D
+from keras.models import Sequential
 
 
 MODEL_NAME = "error_bar"
@@ -53,65 +29,41 @@ x_train, x_test, y_train, y_test = load_data(
     LABELS, TIME_PERIODS, STEP_DISTANCE, LABEL, N_FEATURES, SEED
 )
 
-input_size = x_train.shape[2]
-num_classes = 6
+model = Sequential()
+model.add(
+    Conv1D(160, 12, input_shape=(x_train.shape[1], x_train.shape[2]), activation="relu")
+)
+model.add(Conv1D(128, 10, activation="relu"))
+model.add(Conv1D(96, 8, activation="relu"))
+model.add(Conv1D(64, 6, activation="relu"))
+model.add(GlobalMaxPooling1D())
+model.add(Dropout(0.5))
+model.add(Dense(6, activation="softmax"))
+
+print(model.summary())
+
+model.compile(
+    loss="categorical_crossentropy", optimizer="rmsprop", metrics=["accuracy"]
+)
 
 epochs = 150
-BATCH_SIZE = 1024
-REF_SIZE = 3
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-model = CNN1D(input_size, num_classes).to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.RMSprop(model.parameters())
-
-train = SeqDataset(torch.from_numpy(x_train).float(), torch.from_numpy(y_train).float())
-test = SeqDataset(torch.from_numpy(x_test).float(), torch.from_numpy(y_test).float())
-train_loader = DataLoader(
-    train, batch_size=BATCH_SIZE, shuffle=True, num_workers=os.cpu_count()
-)
-test_loader = DataLoader(
-    test, batch_size=BATCH_SIZE, shuffle=False, num_workers=os.cpu_count()
+batch_size = 1024
+history = model.fit(
+    x_train,
+    y_train,
+    validation_data=(x_test, y_test),
+    epochs=epochs,
+    batch_size=batch_size,
 )
 
-losslist = list()
-model.train()
-best_model = copy.deepcopy(model)
-for epoch in range(epochs):
-    losses = list()
-    for sequences, labels in train_loader:
-        sequences = sequences.to(device)
-        labels = labels.to(device)
-        optimizer.zero_grad()
-        outputs = model(sequences)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
-    ls = np.mean(losses)
-    if epoch > 10:
-        if min(losslist) > ls:
-            best_model = copy.deepcopy(model)
-        # if is_worse(losslist, REF_SIZE, "minimize"):
-        #     break
-    if epoch % 10 == 0:
-        print("Epoch: ", epoch, "Loss: ", loss.item())
-    losslist.append(ls)
 
-model = best_model
-model.eval()
+model.layers[-1].activation = "linear"
 
-model.softmax = nn.Identity()
+model.compile(
+    loss="categorical_crossentropy", optimizer="rmsprop", metrics=["accuracy"]
+)
 
-y_pred = list()
-for batch in test_loader:
-    x, _ = batch
-    x = x.to(device)
-    optimizer.zero_grad()
-    output = model(x)
-    y_pred.append(output.detach().cpu().numpy())
-
-y_pred = np.concatenate(y_pred, axis=0)
+y_pred = model.predict(x_test)
 y_test = y_test.argmax(axis=-1)
 y_pred = pd.DataFrame(y_pred)
 y_pred["true"] = y_test
